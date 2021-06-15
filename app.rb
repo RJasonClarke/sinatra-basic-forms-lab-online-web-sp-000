6454fdfebbd5c6f14f6cedb34cf517ac551c78c3
Robert Clarke <r.jason.clarke@gmail.com>
2021-06-15 14:48:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ class App < Sinatra::Base
     if !Puppy.new
       get "/" do
         erb :index
+      end
     else
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
